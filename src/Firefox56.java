45cc82f1bdc72703ab98dac5d714fa34231d7ce9
HimanshuKhare <himanshukhare1992@gmail.com>
2018-05-17 13:34:37 +0530

    Uploaded Project

---
+++ src/Firefox56.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Firefox56 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Firefox..");
+		
+		System.setProperty("webdriver.gecko.driver", "F:\\PERSONAL\\VIDEO TUTORIALS\\Testing\\Selenium\\Selenium Essentials\\geckodriver-v0.19.0-win64\\geckodriver.exe");
+		
+		WebDriver k = new FirefoxDriver();
+		
+		k.get("https://www.facebook.com");
+		
+		System.out.println(k.getTitle());
+		
+		k.close();
+		
+	}
+}
